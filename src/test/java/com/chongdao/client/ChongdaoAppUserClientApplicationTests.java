31e3a92254019b1fc929aefebba0903b2ea413b0
chongdaoxinxi@163.com <chongdaoxinxi@163.com>
2019-04-18 14:50:04 +0800

    初始化

---
+++ src/test/java/com/chongdao/client/ChongdaoAppUserClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chongdao.client;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChongdaoAppUserClientApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
