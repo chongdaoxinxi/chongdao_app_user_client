f9ed99741507008f5992f451e785d1fa00ed9e4d
chongdaoxinxi@163.com <chongdaoxinxi@163.com>
2019-04-18 17:20:02 +0800

    环境初始化

--- src/main/java/com/chongdao/client/entitys/User.java
+++ src/main/java/com/chongdao/client/entitys/User.java
@@ -1,68 +1,57 @@
 package com.chongdao.client.entitys;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
-import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 import java.util.Date;
 
-
-/**
- * The persistent class for the user database table.
- * 
- */
-@Entity
-@Setter
-@NoArgsConstructor
 @Getter
-@Table(name="user")
-public class User implements Serializable {
-	private static final long serialVersionUID = 1L;
-
+@Setter
+@Entity
+public class User {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    
+
+    /** 用户名 */
     private String name;
-    
-    private String pwd;
-    
-    private Integer status;
-    
-    private Integer type;
-    
+
+    /** 密码 */
+    private String password;
+
+    /** 手机号码 */
+    private String phone;
+
+    /** 头像 */
     private String icon;
-    
+
+    /** 用户标识（微信） */
     private String openId;
-    
-    private String areaCode;
-    
-    private String tel;
-    
-	private BigDecimal money;
-	
+
+    /** 用户余额（充值） */
+    private BigDecimal money;
+
+    /** 集分 */
     private Integer points;
 
-	private Integer createby;
+    private Integer status; //待明确
 
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date createdate;
+    /** 用户类型（1:app用户,2:微信用户） */
+    private Integer type;
 
-	private Integer updateby;
+    /** 用户创建时间 */
+    private Date createTime;
 
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date updatedate;
-	
-	private Integer version;
+    /** 更新时间 */
+    private Date updateTime;
 
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", name=" + name + ", pwd=" + pwd + ", status=" + status + ", type=" + type
-				+ ", icon=" + icon + ", openId=" + openId + ", areaCode=" + areaCode + ", tel=" + tel + ", money="
-				+ money + ", points=" + points + ", createby=" + createby + ", createdate=" + createdate + ", updateby="
-				+ updateby + ", updatedate=" + updatedate + ", version=" + version + "]";
-	}
+    /** 最后一次登录时间（token鉴别有效期使用） */
+    private Date lastLoginTime;
+
+
 }
