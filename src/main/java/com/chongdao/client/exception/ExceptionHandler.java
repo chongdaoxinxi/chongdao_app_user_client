ac9e438faeea51cb5b08f66bf06469738fd284cd
fenglong <1037210165@qq.com>
2019-08-14 17:55:45 +0800

    优化

--- src/main/java/com/chongdao/client/exception/ExceptionHandler.java
+++ src/main/java/com/chongdao/client/exception/ExceptionHandler.java
@@ -24,9 +24,7 @@ public class ExceptionHandler{
     public ResultResponse exceptionHandler(Exception e){
         log.error(e.getMessage(), Method.class);
         e.printStackTrace();
-       if (e instanceof NoHandlerFoundException){
-            return ResultResponse.createByErrorCodeMessage(HttpStatus.SC_NOT_FOUND,ResultEnum.NOT_FOUND.getMessage());
-        }else if (e instanceof MethodArgumentTypeMismatchException){
+       if (e instanceof MethodArgumentTypeMismatchException){
             return ResultResponse.createByErrorCodeMessage(HttpStatus.SC_BAD_REQUEST,
                     "参数类型不匹配,参数" + ((MethodArgumentTypeMismatchException) e).getName()+ "类型应该为" + ((MethodArgumentTypeMismatchException) e).getRequiredType());
         }else if (e instanceof MissingServletRequestParameterException){
@@ -35,8 +33,10 @@ public class ExceptionHandler{
         }else if (e instanceof HttpRequestMethodNotSupportedException) {
             return ResultResponse.createByErrorCodeMessage(HttpStatus.SC_METHOD_NOT_ALLOWED,
                     "不支持" + ((HttpRequestMethodNotSupportedException) e).getMethod() + "方法");
-        }else    if (e instanceof ServletRequestBindingException){
+        }else if (e instanceof ServletRequestBindingException){
            return ResultResponse.createByErrorCodeMessage(HttpStatus.SC_NOT_FOUND,ResultEnum.UNKNOWN_ERROR.getMessage());
+        }else if (e instanceof NoHandlerFoundException){
+            return ResultResponse.createByErrorCodeMessage(HttpStatus.SC_NOT_FOUND,ResultEnum.NOT_FOUND.getMessage());
         }else {
             return ResultResponse.createByErrorCodeMessage(HttpStatus.SC_INTERNAL_SERVER_ERROR, ResultEnum.ERROR.getMessage() );
         }
