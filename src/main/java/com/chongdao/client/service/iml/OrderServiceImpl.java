35341d8e2742901aaece34158078ce077d9807ef
fenglong <1037210165@qq.com>
2019-08-27 17:51:21 +0800

    订单详情

--- src/main/java/com/chongdao/client/service/iml/OrderServiceImpl.java
+++ src/main/java/com/chongdao/client/service/iml/OrderServiceImpl.java
@@ -276,16 +276,11 @@ public class OrderServiceImpl extends CommonRepository implements OrderService{
             if (couponInfo != null) {
                 //满减
                 if (couponInfo.getCpnType() == 4){
-                    CpnThresholdRule cpnThresholdRule = cpnThresholdRuleRepository.findByCpnId(couponInfo.getId());
                     orderVo.setFullCouponName(couponInfo.getCpnName());
-                    orderVo.setFullCouponPrice(cpnThresholdRule.getMinPrice());
-                }else if (couponInfo.getCpnType() == 1){ //红包
+                    orderVo.setFullCouponPrice(couponInfo.getCpnValue());
+                }else{ //红包
                     orderVo.setCouponName(couponInfo.getCpnName());
                     orderVo.setCouponPrice(couponInfo.getCpnValue());
-                }else{ //优惠券
-                    CpnThresholdRule cpnThresholdRule = cpnThresholdRuleRepository.findByCpnId(couponInfo.getId());
-                    orderVo.setCouponName(couponInfo.getCpnName());
-                    orderVo.setCouponPrice(cpnThresholdRule.getMinPrice());
                 }
             }
         }
