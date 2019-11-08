91bc8c17dbf5beb59178095e6de4ebf889e652d4
LevyLii <782712046@163.com>
2019-11-08 15:32:48 +0800

    bug修改

--- src/main/java/com/chongdao/client/service/iml/OrderServiceImpl.java
+++ src/main/java/com/chongdao/client/service/iml/OrderServiceImpl.java
@@ -214,8 +214,8 @@ public class OrderServiceImpl extends CommonRepository implements OrderService {
         //如果orderType为2代表提交订单 3代表拼单
         if (orderCommonVO.getOrderType() == OrderStatusEnum.ORDER_CREATE.getStatus() || orderCommonVO.getOrderType() == OrderStatusEnum.ORDER_SPELL.getStatus()) {
             //地址判断
-            if (orderCommonVO.getServiceType() != 3 && (orderCommonVO.getReceiveAddressId() == null) && orderCommonVO.getReceiveTime() == null) {
-                if (orderCommonVO.getServiceType() == 1 && orderVo.getDeliverTime() == null) { //双程
+            if (orderCommonVO.getServiceType() != 3 && (orderCommonVO.getReceiveAddressId() == null)) {
+                if (orderCommonVO.getServiceType() == 1 && orderVo.getDeliverTime() == null && orderCommonVO.getDeliverAddressId() == null) { //双程
                     return ResultResponse.createByErrorCodeMessage(GoodsStatusEnum.ADDRESS_EMPTY.getStatus(), GoodsStatusEnum.ADDRESS_EMPTY.getMessage());
                 }
                 return ResultResponse.createByErrorCodeMessage(GoodsStatusEnum.ADDRESS_EMPTY.getStatus(), GoodsStatusEnum.ADDRESS_EMPTY.getMessage());
