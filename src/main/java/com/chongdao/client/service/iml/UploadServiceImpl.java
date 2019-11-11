0befc5acaf66d7d3c4c74ce8357efc884d6db89d
onlineS <start2016shi@gmail.com>
2019-11-11 15:51:13 +0800

    将老商家老商品的图片 转换上传到新服务器

--- src/main/java/com/chongdao/client/service/iml/UploadServiceImpl.java
+++ src/main/java/com/chongdao/client/service/iml/UploadServiceImpl.java
@@ -1,7 +1,11 @@
 package com.chongdao.client.service.iml;
 
 import com.chongdao.client.common.ResultResponse;
+import com.chongdao.client.entitys.Good;
+import com.chongdao.client.entitys.Shop;
 import com.chongdao.client.enums.ResultEnum;
+import com.chongdao.client.repository.GoodsRepository;
+import com.chongdao.client.repository.ShopRepository;
 import com.chongdao.client.service.UploadService;
 import com.github.tobato.fastdfs.domain.StorePath;
 import com.github.tobato.fastdfs.proto.storage.DownloadByteArray;
@@ -11,14 +15,15 @@ import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 import java.net.URLEncoder;
+import java.util.List;
 
 @Service
 @Slf4j
@@ -29,6 +34,14 @@ public class UploadServiceImpl implements UploadService {
 
     @Value("${fdfs.host}")
     private String hostAddress;
+
+    @Value("${fdfs.url}")
+    private String url;
+
+    @Autowired
+    private ShopRepository shopRepository;
+    @Autowired
+    private GoodsRepository goodsRepository;
 
 
     @Override
@@ -47,6 +60,90 @@ public class UploadServiceImpl implements UploadService {
             log.error("【上传图片】上传失败，error={}", e.getMessage());
             return ResultResponse.createByErrorMessage(e.getMessage());
         }
+    }
+
+    @Override
+    public ResultResponse convertOldImageToNew() throws IOException {
+        convertShopImage();
+        return null;
+    }
+
+    private void convertShopImage() {
+        String oldUrl = "";
+        List<Shop> all = shopRepository.findAll();
+        for(Shop s : all) {
+            String logo = s.getLogo();
+            if(StringUtils.isNotBlank(logo)) {
+                if(logo.indexOf("http") == -1) {
+                    oldUrl = logo;
+                    oldUrl = "https://www.chongdaopet.com/images/" + oldUrl;
+                    String fileName = oldUrl.substring(oldUrl.lastIndexOf("/") + 1);
+                    String tempPath = "F:\\temp_image";
+                    //根据下载链接, 将图片下载存储到服务器上, 并保存访问url
+                    RestTemplate rest = new RestTemplate();
+                    rest.execute(oldUrl, HttpMethod.GET, (req) -> {
+                    }, (res) -> {
+                        InputStream inputStream = res.getBody();
+                        FileOutputStream out = new FileOutputStream(tempPath + "\\" + fileName);
+                        int byteCount = 0;
+                        while ((byteCount = inputStream.read()) != -1) {
+                            out.write(byteCount);
+                        }
+                        out.close();
+                        inputStream.close();
+                        File file = new File(tempPath + "\\" + fileName);
+                        FileInputStream in = new FileInputStream(file);
+                        StorePath storePath = this.storageClient.uploadFile(in, file.length(), FilenameUtils.getExtension(fileName), null);
+                        String visitUrl = "http://" + url + "/" + storePath.getFullPath();
+                        System.out.println("visitUrl:    " + visitUrl);
+                        s.setLogo(visitUrl);
+                        shopRepository.save(s);
+                        in.close();
+                        return null;
+                    });
+                }
+            }
+        }
+        System.out.println("convert shop logo complete!");
+    }
+
+    private void conertGoodImage() {
+        String oldUrl = "";
+        List<Good> all = goodsRepository.findAll();
+        for(Good g : all) {
+            String icon = g.getIcon();
+            if(StringUtils.isNotBlank(icon)) {
+                if(icon.indexOf("http") == -1) {
+                    oldUrl = icon;
+                    oldUrl = "https://www.chongdaopet.com/images/" + oldUrl;
+                    String fileName = oldUrl.substring(oldUrl.lastIndexOf("/") + 1);
+                    String tempPath = "F:\\temp_image";
+                    //根据下载链接, 将图片下载存储到服务器上, 并保存访问url
+                    RestTemplate rest = new RestTemplate();
+                    rest.execute(oldUrl, HttpMethod.GET, (req) -> {
+                    }, (res) -> {
+                        InputStream inputStream = res.getBody();
+                        FileOutputStream out = new FileOutputStream(tempPath + "\\" + fileName);
+                        int byteCount = 0;
+                        while ((byteCount = inputStream.read()) != -1) {
+                            out.write(byteCount);
+                        }
+                        out.close();
+                        inputStream.close();
+                        File file = new File(tempPath + "\\" + fileName);
+                        FileInputStream in = new FileInputStream(file);
+                        StorePath storePath = this.storageClient.uploadFile(in, file.length(), FilenameUtils.getExtension(fileName), null);
+                        String visitUrl = "http://" + url + "/" + storePath.getFullPath();
+                        System.out.println("visitUrl:    " + visitUrl);
+                        g.setIcon(visitUrl);
+                        goodsRepository.save(g);
+                        in.close();
+                        return null;
+                    });
+                }
+            }
+        }
+        System.out.println("convert goods icon complete!");
     }
 
     /**
